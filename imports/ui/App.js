2b3b1316d464a0c604a1dd8134f408bac231bd92
Ageon <denis.sytchenko@nova-test.ru>
2019-12-01 00:00:17 +0300

    added filtering data with publish and subscribe

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -43,9 +43,18 @@ class App extends Component {
             filteredTasks = filteredTasks.filter(task => !task.checked);
         }
 
-        return this.props.tasks.map((task) => (
-            <Task key={task._id} task={task} />
-        ));
+        return filteredTasks.map((task) => {
+            const currentUserId = this.props.currentUser && this.props.currentUser._id;
+            const showPrivateButton = task.owner === currentUserId;
+
+            return (
+                <Task
+                    key={task._id}
+                    task={task}
+                    showPrivateButton={showPrivateButton}
+                />
+            );
+        });
     }
 
     render() {
@@ -86,6 +95,8 @@ class App extends Component {
 }
 
 export default withTracker(() => {
+    Meteor.subscribe('tasks');
+
     return {
         tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
         incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Tasks } from '../api/tasks.js';
+import classnames from 'classnames';
 
 // Task component - represents a single todo item
 export default class Task extends Component {
@@ -17,10 +18,17 @@ export default class Task extends Component {
         return userName ? userName : 'Anonimous';
     }
 
+    togglePrivate() {
+        Meteor.call('tasks.setPrivate', this.props.task._id, ! this.props.task.private);
+    }
+
     render() {
         // Give tasks a different className when they are checked off,
         // so that we can style them nicely in CSS
-        const taskClassName = this.props.task.checked ? 'checked' : '';
+        const taskClassName = classnames({
+            checked: this.props.task.checked,
+            private: this.props.task.private,
+        });
 
         return (
             <li className={taskClassName}>
@@ -34,6 +42,12 @@ export default class Task extends Component {
                     checked={!!this.props.task.checked}
                     onClick={this.toggleChecked.bind(this)}
                 />
+
+                { this.props.showPrivateButton ? (
+                    <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
+                        { this.props.task.private ? 'Private' : 'Public' }
+                    </button>
+                ) : ''}
 
                 <span className="text">
                     <strong>{this.setAnonymity(this.props.task.username)}</strong>: {this.props.task.text}
